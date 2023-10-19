4f4fb0a9e1ad2c1abc1d230c2a320da396a332f4
Nathan Toombs <ntoombs19@gmail.com>
2023-10-19 11:01:02 -0500

    test routing

---
+++ pages/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/project/1">Project 1</NuxtLink></li>
+        <li><NuxtLink to="/project/2">Project 2</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
