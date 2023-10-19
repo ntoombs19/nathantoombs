c239a969eaa86b7a75f52e3093ce8eacf9cce628
Nathan Toombs <ntoombs19@gmail.com>
2023-10-19 11:29:38 -0500

    Fix page links

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <nav>
       <ul>
         <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/project/1">Project 1</NuxtLink></li>
-        <li><NuxtLink to="/project/2">Project 2</NuxtLink></li>
+        <li><NuxtLink to="/projects/1">Project 1</NuxtLink></li>
+        <li><NuxtLink to="/projects/2">Project 2</NuxtLink></li>
       </ul>
     </nav>
   </header>
